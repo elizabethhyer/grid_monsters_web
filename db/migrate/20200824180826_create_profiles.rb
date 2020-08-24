e1c0ce605d2b63272c893ffabba3fad6d22fd94f
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-08-24 16:28:53 -0400

    Adds columns to user and profile tables

--- db/migrate/20200824180826_create_profiles.rb
+++ db/migrate/20200824180826_create_profiles.rb
@@ -1,8 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-
-      t.timestamps null: false
+      t.string :name
+      t.string :species
+      t.text :bio
     end
   end
 end
