3fca4fbdc0669241b27e12edee8be8294d8839e5
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-08-24 13:53:42 -0400

    Adds login, signup, home and failure erb files and routes in the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,59 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :home
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect "/failure"
+    else 
+      User.create(username: params[:username], password: params[:password])
+      redirect "/home"
+    end 
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :home
+  end
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && 
+      user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/home"
+    else 
+      redirect "/failure"
+    end 
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
