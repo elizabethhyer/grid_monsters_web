be3b23f62c8a68533260bae57f8257232b6725a6
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-08-24 17:24:36 -0400

    Sets up profile/index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,85 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get "/signup" do
+    erb :'users/signup'
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect "/failure"
+    else 
+      User.create(username: params[:username], password: params[:password])
+      redirect "/"
+    end 
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :home
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
+  get "/login" do
+    erb :'users/login'
   end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && 
+      user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/home"
+    else 
+      redirect "/failure"
+    end 
   end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
+  get "/failure" do
+    erb :'users/failure'
   end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
+  get "/logout" do
+    session.clear
+    redirect "/"
   end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+  # # GET: /users/new
+  # get "/users/new" do
+  #   erb :"/users/new.html"
+  # end
+
+  # # POST: /users
+  # post "/users" do
+  #   redirect "/users"
+  # end
+
+  # # GET: /users/5
+  # get "/users/:id" do
+  #   erb :"/users/show.html"
+  # end
+
+  # # GET: /users/5/edit
+  # get "/users/:id/edit" do
+  #   erb :"/users/edit.html"
+  # end
+
+  # # PATCH: /users/5
+  # patch "/users/:id" do
+  #   redirect "/users/:id"
+  # end
+
+  # # DELETE: /users/5/delete
+  # delete "/users/:id/delete" do
+  #   redirect "/users"
+  # end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20200824180826) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "password_digest"
   end
 
