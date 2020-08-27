3a6d5bbbf7d171bff5311f4139257b63e42113dc
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-08-27 14:02:29 -0400

    Adds name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200827032206) do
+ActiveRecord::Schema.define(version: 20200827180117) do
 
   create_table "profiles", force: :cascade do |t|
     t.string  "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20200827032206) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "name"
   end
 
 end
